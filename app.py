592e8900237d8f89e437b27cdacb05698397a568
Artem <tu_artem@mail.ru>
2019-03-02 14:22:24 +0200

    flask app works from r and python

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 import pandas as pd
 from flask import Flask, request, jsonify
+from catboost import CatBoostClassifier
 import pickle
 import json
 app = Flask(__name__)
 
 # Load the model
-with open("models/ohe.pcl", "rb") as f:
-    ohe = pickle.load(f)
+model = CatBoostClassifier()
+model.load_model("models/model.cbm")
 
-with open("models/model.pcl", "rb") as f:
-    model = pickle.load(f)
+with open("models/transformer.pcl", "rb") as f:
+    transformer = pickle.load(f)
 
-@app.route('/api',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request.
+    print(request)
     data = request.get_json(force=True)
+    app.logger.info(data)
     # Make prediction using model loaded from disk as per the data.
-    df = pd.DataFrame(json.loads(data), index=[0])
-    enc = ohe.transform(df)
-    prediction = model.predict_proba(enc)
+    #df = pd.DataFrame(json.loads(data), index=[0])
+    df = pd.DataFrame(data, index=[0])
+
+    data_array = transformer.transform(df).toarray()
+
+    prediction = model.predict_proba(data_array)
     # Take the first value of prediction
     output = prediction[0]
     return jsonify(list(output))
--- train.py
+++ train.py
@@ -1,12 +1,16 @@
-import pandas as pd
-import numpy as np
+import pickle
 
+import pandas as pd
+#import numpy as np
+
+from sklearn.preprocessing import OneHotEncoder, Normalizer
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, roc_auc_score
 
 from catboost import CatBoostClassifier
 
-SAVE_MODELS = False
+SAVE_MODELS = True
 
 data = pd.read_csv("data/raw/Chapter_3_Diabetes_data.csv", low_memory=False)
 
@@ -55,15 +59,27 @@ x_train, x_test, y_train, y_test = train_test_split(
     shuffle=False,
     test_size=0.30)
 
-categorical_features = np.where(x_train.dtypes != "int64")[0]
+# categorical_features = np.where(x_train.dtypes != "int64")[0]
+
+cat = [t != "int64" for t in x_train.dtypes]
+num = [t == "int64" for t in x_train.dtypes]
+
+transformer = ColumnTransformer(
+    [("cat",  OneHotEncoder(handle_unknown="ignore"), cat),
+     ("num", Normalizer(), num)]
+)
+
+x_train = transformer.fit_transform(x_train)
+x_test = transformer.transform(x_test)
+
 
 model = CatBoostClassifier(
     iterations=100,
-    depth=3,
+    depth=5,
     learning_rate=0.1,
     loss_function='Logloss',
     logging_level='Verbose',
-    cat_features=categorical_features,
+   # cat_features=categorical_features,
     early_stopping_rounds=10,
     l2_leaf_reg=0.1
     )
@@ -72,9 +88,9 @@ model = CatBoostClassifier(
 y_train_int = [int(x) for x in y_train.to_list()]
 y_test_int = [int(x) for x in y_test.to_list()]
 
-model.fit(x_train,
+model.fit(x_train.toarray(),
           y_train_int,
-          eval_set=(x_test, y_test_int)
+          eval_set=(x_test.toarray(), y_test_int)
           )
 
 # y_train_int = [int(x) for x in y_train.to_list()]
@@ -82,7 +98,14 @@ model.fit(x_train,
 
 # model.fit(x_train.toarray(), y_train_int)
 
-print(roc_auc_score(y_test.to_list(), model.predict_proba(x_test)[:, 1]))
-print(accuracy_score(y_test.to_list(), model.predict(x_test)))
+print(roc_auc_score(y_test.to_list(), model.predict_proba(x_test.toarray())[:, 1]))
+print(accuracy_score(y_test.to_list(), model.predict(x_test.toarray())))
 # print(accuracy_score(y_train_int, model.predict(x_train.toarray())))
 # print(accuracy_score(y_test_int, model.predict(x_test.toarray())))
+
+
+if SAVE_MODELS:
+    model.save_model("models/model.cbm")
+
+    with open("models/transformer.pcl", "wb") as f:
+        pickle.dump(transformer, f)
