d71018a93485eac375de0dedcfee561d982e423c
Artem <tu_artem@mail.ru>
2019-03-03 20:37:51 +0200

    try on lime

--- app.py
+++ app.py
@@ -1,34 +1,61 @@
 import pandas as pd
+import numpy as np
 from flask import Flask, request, jsonify
-from catboost import CatBoostClassifier
+from xgboost import XGBClassifier
 import pickle
 import json
+from lime import lime_tabular
+
 app = Flask(__name__)
 
 # Load the model
-model = CatBoostClassifier()
-model.load_model("models/model.cbm")
+model = XGBClassifier()
+model.load_model("models/model.xgb")
+x_train = np.load("data/processed/x_train.npy")
+with open("data/processed/features.json") as f:
+    all_feature_names = json.load(f)
+
 
 with open("models/transformer.pcl", "rb") as f:
     transformer = pickle.load(f)
 
+
+explainer = lime_tabular.LimeTabularExplainer(
+    x_train,
+    feature_names=all_feature_names,
+    class_names=["NO", "YES"],
+    discretize_continuous=True
+    )
+
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request.
-    print(request)
     data = request.get_json(force=True)
-    app.logger.info(data)
-    # Make prediction using model loaded from disk as per the data.
-    #df = pd.DataFrame(json.loads(data), index=[0])
+    # app.logger.info(data)
     df = pd.DataFrame(data, index=[0])
-
     data_array = transformer.transform(df).toarray()
 
     prediction = model.predict_proba(data_array)
     # Take the first value of prediction
-    output = prediction[0]
-    return jsonify(list(output))
+    output = prediction[0].tolist()
+    return jsonify(output)
 
+
+@app.route("/explain", methods=["POST"])
+def explain():
+    data = request.get_json(force=True)
+    # app.logger.info(data)
+    df = pd.DataFrame(data, index=[0])
+    data_array = transformer.transform(df)
+    exp = explainer.explain_instance(
+        data_array.toarray()[0],
+        model.predict_proba,
+        num_features=5,
+        top_labels=2
+        )
+
+    output = exp.as_list()
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
